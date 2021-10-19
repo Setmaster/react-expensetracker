5a5776951cf121af964b7236e6ef40b0c494498d
Victor <vi7org@hotmail.com>
2021-10-19 19:25:11 -0400

    The Expenses component now uses the Card component as a wrapper

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,16 +1,17 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 
 const Expenses = (props) => {
     const expenses = props.expenses;
     return (
-        <div className={"expenses"}>
+        <Card className={"expenses"}>
             <h2>Let's get started!</h2>
             <ExpenseItem data={expenses[0]}/>
             <ExpenseItem data={expenses[1]}/>
             <ExpenseItem data={expenses[2]}/>
             <ExpenseItem data={expenses[3]}/>
-        </div>
+        </Card>
     );
 
 }

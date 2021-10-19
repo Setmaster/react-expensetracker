91e86826e99177bc285c08c9db75b91248d1a2cd
Victor <vi7org@hotmail.com>
2021-10-19 19:23:48 -0400

    Card now accepts classes from its parent

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,9 @@
 import "./Card.css";
 
 const Card = (props) => {
-
+    const classes = 'card ' + props.className;
     return (
-        <div className={"card"}>{props.children}</div>
+        <div className={classes}>{props.children}</div>
     );
 }
 

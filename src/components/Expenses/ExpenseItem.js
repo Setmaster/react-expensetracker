ad1d5fb0b6265f1d14655e9f5e4f481556c55578
Victor <vi7org@hotmail.com>
2021-10-20 10:08:44 -0400

    Added change title button to ExpenseItem and function to handle clicks

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,17 +6,21 @@ const ExpenseItem = (props) => {
 
     const title = props.data.title;
     const amount = props.data.amount;
+    const clickHandler = () => {
+        console.log("Clicked");
+    }
+
 
     return (
         <Card className={"expense-item"}>
             <ExpenseDate
             date={props.data.date}
             />
-
             <div className={"expense-item__description"}>
                 <h2>{title}</h2>
                 <div className={"expense-item__price"}>${amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     )
 }

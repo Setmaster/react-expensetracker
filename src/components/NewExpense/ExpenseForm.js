f87e18c1d18b1b377ce49f84ae1cf5deb0b596e3
Victor <vi7org@hotmail.com>
2021-10-20 22:15:18 -0400

    Reverted back to individual useStates

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,33 +2,18 @@ import './ExpenseForm.css'
 import {useState} from "react";
 
 const ExpenseForm = () => {
-        const [userInput, setUserInput] = useState({
-            enteredTitle: '',
-            enteredAmount: '',
-            enteredDate: ''
-        });
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
 
     const titleChangeHandler = (event) => {
-        // setUserInput({...userInput, enteredTitle: event.target.value});
-        setUserInput((prevState)=>{
-            return(
-                {...prevState, enteredTitle: event.target.value}
-            )
-        });
+         setEnteredTitle(event.target.value);
     };
     const amountChangeHandler = (event) => {
-        setUserInput((prevState)=>{
-            return(
-                {...prevState, enteredAmount: event.target.value}
-            )
-        });
+        setEnteredAmount(event.target.value);
     };
     const dateChangeHandler = (event) => {
-        setUserInput((prevState)=>{
-            return(
-                {...prevState, enteredDate: event.target.value}
-            )
-        });
+        setEnteredDate(event.target.value);
     };
 
 

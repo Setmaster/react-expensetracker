5de618be6776fcd285f7c131284d69ed2232036b
Victor <vi7org@hotmail.com>
2021-10-21 22:28:32 -0400

    Added ExpenseFilter component, will handle filtering of expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,24 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpenseFilter from "../NewExpense/ExpenseFilter";
+import {useState} from "react";
 
 const Expenses = (props) => {
-    const expenses = props.expenses;
+    const {expenses} = props;
+
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = (event) => {
+        setFilteredYear(event.target.value);
+        console.log(event.target.value);
+    };
+
     return (
+        <div>
+            <ExpenseFilter
+            onChangeFilter={filterChangeHandler}
+            />
         <Card className={"expenses"}>
             <h2>Let's get started!</h2>
             <ExpenseItem data={expenses[0]}/>
@@ -12,6 +26,7 @@ const Expenses = (props) => {
             <ExpenseItem data={expenses[2]}/>
             <ExpenseItem data={expenses[3]}/>
         </Card>
+        </div>
     );
 
 }
